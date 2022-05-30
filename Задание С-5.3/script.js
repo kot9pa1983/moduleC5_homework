b5302480e350487bc0186ec205b1db517aa2e00d
kot9pa1983 <98043790+kot9pa1983@users.noreply.github.com>
2022-05-30 15:12:49 +0500

    Add files via upload

---
+++ Задание С-5.3/script.js
@@ -0,0 +1,67 @@
+
+function useRequest(url, callback) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  
+  xhr.onload = function() {
+    if (xhr.status != 200) {
+      console.log('Статус ответа: ', xhr.status);
+    } else {
+      const result = JSON.parse(xhr.response);
+      if (callback) {
+        callback(result);
+      }
+    }
+  };
+  
+  xhr.onerror = function() {
+    console.log('Ошибка! Статус ответа: ', xhr.status);
+  };
+  
+  xhr.send();
+};
+
+const resultNode = document.querySelector('.j-result');
+const btnNode = document.querySelector('.j-btn-request');
+
+
+function getIndex() {
+  
+index = document.getElementsByTagName("input")[0].value
+
+if (index >= 1 && index <= 10) {
+  userHtml = 'https://picsum.photos/v2/list/?limit=' + index;}
+  
+  else {alert('Число вне диапазона от 1 до 10')
+}
+
+return userHtml;
+}
+
+btnNode.addEventListener('click', () => {
+  getIndex();
+})
+
+
+function displayResult(apiData) {
+  let cards = '';
+  apiData.forEach(item => {
+    const cardBlock = `
+      <div class="card">
+        <img
+          src="${item.download_url}"
+          class="card-image"
+        />
+        <p>${item.author}</p>
+      </div>
+    `;
+    cards = cards + cardBlock;
+  });
+  
+    
+  resultNode.innerHTML = cards;
+}
+
+btnNode.addEventListener('click', () => {
+  useRequest(userHtml, displayResult);
+})
